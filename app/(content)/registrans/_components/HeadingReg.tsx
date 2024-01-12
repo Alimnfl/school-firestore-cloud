d2db9edee81d8301e700e1c3509d2960d3f88d3f
Alimnfl <mnaufallim234@gmail.com>
2024-01-12 09:46:29 +0700

    Added HeadingRegistration

---
+++ app/(content)/registrans/_components/HeadingReg.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HeadingReg() {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-xl sm:text-3xl md:text-4xl font-bold">
+        <span className="underline">Dreamer Student Registrant!</span>
+      </h1>
+    </div>
+  );
+}
+
+export default HeadingReg;
